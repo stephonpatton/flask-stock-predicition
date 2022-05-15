86fb50e1e6de331f0a881c33057cd823e82260a0
Stephon Patton <stephonpatton@icloud.com>
2022-05-15 08:03:37 -0500

    - Added candlestick chart back to project instead of a Scatterplot

--- src/pages/Apple.jsx
+++ src/pages/Apple.jsx
@@ -22,9 +22,10 @@ import {
     Bar
 } from 'recharts';
 import queryString from "query-string";
+// import MaterialTable from "material-table";
+
+
 import aapl from '../stock_json/aapl.json'
-
-
 
 const aaplData = 'https://yahoo-finance-api.vercel.app/AAPL';
 async function getData() {
@@ -32,18 +33,11 @@ async function getData() {
     return response.json();
 }
 
-
-
-
-
 const API_TOKEN = "c9mrn1qad3ie533htcn0";
 const STOCK_SYMBOLS = ["AAPL"]
 const INTERVAL_OPTIONS = [30, 60, 90, 120]
 const API_URL = "https://finnhub.io/api/v1/stock/candle"
 const RESOLUTION = "D"
-
-
-
 
 function getUnixTime(date) {
     return date.getTime() / 1000 | 0;
@@ -55,7 +49,6 @@ const directionEmojis = {
     '': '',
 };
 
-
 const chart = {
     options: {
         chart: {
@@ -63,7 +56,7 @@ const chart = {
             height: 350
         },
         title: {
-            text: 'Apple Chart',
+            text: 'Apple Daily Candle Chart',
             align: 'left'
         },
         xaxis: {
@@ -81,12 +74,10 @@ const round = (number) => {
     return number ? +(number.toFixed(2)) : null;
 };
 
-const datas = [{name: 'Page A', uv: 200, pv: 2400, amt: 2400}];
-
 function Apple() {
     const [symbol, setSymbol] = useState(STOCK_SYMBOLS[0])
     const [data, setData] = useState(null);
-    const [interval, setInterval] = useState(INTERVAL_OPTIONS[0]);
+    const [interval, setInterval] = useState(INTERVAL_OPTIONS[3]);
 
     const to = useMemo(() => {
         return getUnixTime(new Date());
@@ -116,14 +107,11 @@ function Apple() {
             .catch(error => console.error(error))
     }, [from, to, symbol]);
 
-
-
     const handleChangeStock = event =>
         setSymbol(event.target.value);
 
     const handleChangeInterval = event =>
         setInterval(event.target.value);
-
 
     const [series, setSeries] = useState([{
         data: []
@@ -134,45 +122,27 @@ function Apple() {
     const [priceTime, setPriceTime] = useState(null);
 
     function addPredictionData(data) {
-        // console.log(aapl.t)
-        // console.log(aapl.c)
-        // data['pc'].push(2120);
         const predicted = [];
         const predTimestamps = [];
-        for(let i = 0; i < data.c.length - 1; i++){
+        for(let i = 0; i < data.c.length; i++){
             predicted.push(data.c[i]);
         }
 
         for(let i = 0; i < 30; i++) {
-            if(data.t[i] === aapl.t[i]) {
-                predicted.push(aapl.c[i]);
-                i++;
-            } else {
-                predicted.push(aapl.c[i]);
-                predTimestamps.push(aapl.t[i]);
-            }
+            predicted.push(aapl.c[i]);
+            predTimestamps.push(aapl.t[i]);
         }
         data.pc = predicted;
         data.pt = predTimestamps;
         console.log(data)
         for(let i = 0; i < 30; i++) {
             data.c.push(aapl.c[i]);
-            data.t.push(data.pt[i]);
-            // data.t.push((new Date().setDate(new Date().getDate() + i)/1000))
+            data.t.push(aapl.t[i]);
         }
-
-
     }
 
     function transformData(data) {
         addPredictionData(data)
-        // data.c.push(20,30,40);
-        // data.t.push(new Date().getTime()/1000, new Date().setDate(new Date().getDate() + 1)/1000, new Date().setDate(new Date().getDate() + 2)/1000);
-        // for(let i = data.c.length - 30; i < data.c.length; i++) {
-        //     return data.c.map((item, i) => ({
-        //         predictedClose: Number(data.c[i]).toFixed(2)
-        //     }))
-        // }
         return data.c.map((item, index) => ({
             close: Number(item).toFixed(2),
             open: Number(data.o[index]).toFixed(2),
@@ -182,9 +152,6 @@ function Apple() {
             volume: Number(data.v[index])
         }))
     }
-
-
-
 
     useEffect(() => {
         let timeoutId;
@@ -219,17 +186,13 @@ function Apple() {
 
     const direction = useMemo(() => prevPrice < price ? 'up' : prevPrice > price ? 'down' : '', [prevPrice, price]);
 
-
     return (
         <div>
             <DropdownMenu/>
             <div className={['price', direction].join(' ')}>
                 ${price} {directionEmojis[direction]}
             </div>
-            {/*<div className="price-time">*/}
-            {/*    {priceTime && priceTime.toLocaleTimeString()}*/}
-            {/*</div>*/}
-
+            <Chart options={chart.options} series={series} type="candlestick" width="100%" height={450} />
 
             <div className="container">
                 <div className="selector">
@@ -250,27 +213,23 @@ function Apple() {
                 </div>
             </div>
 
-
             <ResponsiveContainer width="100%" height={300}>
                 <LineChart data={data}
                            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
                     <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="timestamp" />
-                    {/*<XAxis dataKey="predictTime" />*/}
+                    <XAxis dataKey="timestamp"/>
                     <YAxis type="number" allowDecimals={true}
-                           allowDataOverflow={true} />
+                           allowDataOverflow={true} domain={[130, 180]}/>
                     <Tooltip />
                     <Legend />
-                    <Line type="monotone" strokeWidth={1.4} dataKey="open" stroke="blue" dot={false} />
-                    {/*<Line type="monotone" dataKey="close" stroke="green" dot={false} />*/}
-                    <Line type="monotone" dataKey="predictions" stroke="red" dot={false} />
-                    {/*<Line type="monotone" dataKey="volume" stroke="red" dot={false} />*/}
+                    <Line type="monotone" strokeWidth={1.4} dataKey="predictions" stroke="red" dot={false} />
+                    <Line type="monotone" dataKey="open" strokeWidth={2} stroke="green" dot={false} />
                 </LineChart>
             </ResponsiveContainer>
 
             <ResponsiveContainer width="100%" height={300}>
                 <BarChart
-                    // width={500}
+                    width={500}
                     height={300}
                     data={data}
                     margin={{
@@ -291,29 +250,7 @@ function Apple() {
                 </BarChart>
             </ResponsiveContainer>
 
-            <ResponsiveContainer width="100%" height="100%">
-                <ScatterChart
-                    width={400}
-                    height={400}
-                    data={data}
-                    margin={{
-                        top: 20,
-                        right: 20,
-                        bottom: 20,
-                        left: 20,
 
-                    }}
-                >
-                    {/*// TODO: Figure out why this shit ain't working cuhhhhh*/}
-                    <CartesianGrid />
-                    <XAxis type="number" dataKey="timestamp" />
-                    <YAxis type="number" dataKey="close"/>
-                    <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-                    <Scatter name="A school" fill="#8884d8" />
-                </ScatterChart>
-            </ResponsiveContainer>
-
-            {/*<Chart options={chart.options} series={series} type="candlestick" width="100%" height={450} />*/}
         </div>
 
 
